1af13d4aef3c391f1da7d19c0b2a36d0f79858c9
malte <malte.lindkvist@elev.ga.ntig.se>
2023-08-31 11:01:10 +0200

    ✨ variabel kod

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 console.log("Tjabba tjenare")
+
+let firstName
+firstName = "Jens"
+
+let lastName = "Andreasson"
+
+let num = 12
+
+console.log(typeof(num))
+
+
+const NAME = "Stina"
+console.log(NAME)
+for (let i = 0; i < 10; i++) {
+    const NAME = "Sture"
+console.log(NAME)
+}
+
+var skola = "NTI"
+console.log(skola)
+
+
+let a = "Malte"
+let b = `Malte`
+let c = `Mitt namn är ${a}`
+let d = 'Mitt namn är  '+ b
+
+let e = 12 
+let f = 16.7
+
+let g = true
+let h = false
